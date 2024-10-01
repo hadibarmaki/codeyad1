7a31bcef147036ba69db02d9414159dc5b2f35ef
Hadi Barmaki <hadibarmaki75@gmail.com>
2024-10-01 12:50:51 -0700

    debug Project model

--- project_app/models.py
+++ project_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Project(models.model):
+class Project(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=150)
     address = models.CharField(max_length=150)
