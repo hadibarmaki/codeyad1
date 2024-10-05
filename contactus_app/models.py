714504e735a3494553ca62a63dfaa74fac3f19e6
Hadi Barmaki <hadibarmaki75@gmail.com>
2024-10-05 13:52:20 -0700

    create Message model and add it to the project template

--- contactus_app/models.py
+++ contactus_app/models.py
@@ -11,3 +11,11 @@ class Footer(models.Model):
     telegram = models.CharField(max_length=50)
     instagram = models.CharField(max_length=50)
     whatsapp = models.CharField(max_length=50)
+
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    subject = models.CharField(max_length=50)
+    body = models.TextField()
--- project_app/views.py
+++ project_app/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import Project
-from contactus_app.models import Footer
+from contactus_app.models import Footer,Message
 
 # Create your views here.
 
 def home(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        sub = request.POST.get('subject')
+        body = request.POST.get('body')
+        Message.objects.create(name=name, email=email, subject=sub, body=body)
+
     projects = Project.objects.all()
     contact = Footer.objects.all().last()
     return render(request, 'index.html', context={'projects':projects, 'contact': contact})
