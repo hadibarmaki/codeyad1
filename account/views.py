f67ae17bb910757a4db4847a0543db8c5aaef89b
Hadi Barmaki <hadibarmaki75@gmail.com>
2024-10-11 08:58:08 -0700

    correct and standard addressing

--- account/views.py
+++ account/views.py
@@ -6,14 +6,14 @@ from django.contrib.auth.models import User
 
 def user_login(request):
     if request.user.is_authenticated == True:
-        return redirect('/')
+        return redirect('main:home')
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('/')
+            return redirect('main:home')
 
     return render(request, 'account/login.html')
 
@@ -21,14 +21,14 @@ def user_login(request):
 
 def user_logout(request):
     logout(request)
-    return redirect('/')
+    return redirect('main:home')
 
 
 
 def user_register(request):
     context = {"errors": []}
     if request.user.is_authenticated == True:
-        return redirect('/')
+        return redirect('main:home')
     if request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
@@ -39,5 +39,5 @@ def user_register(request):
             return render(request, 'account/register.html', context)
         user = User.objects.create(username=username, email=email, password=password1)
         login(request, user)
-        return redirect('/')
+        return redirect('main:home')
     return render(request, 'account/register.html')
